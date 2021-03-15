792d74bc9c0113dd9dd67e0c43cd13c6ac6c3dd7
Jakub Kovář <jakub.kovar@stuba.sk>
2021-03-15 13:39:22 +0100

    enum methods and attributes

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -3,14 +3,14 @@ package sk.stuba.fei.uim.oop;
 public class Main {
     public static void main(String[] args) {
 
-        checkSever(SvetoveStrany.valueOf("SEVER"));
-        checkSever(SvetoveStrany.valueOf("JUH"));
-        checkSever(SvetoveStrany.valueOf("sever"));
+        checkSever(SvetoveStrany.fromString("SEVER"));
+        checkSever(SvetoveStrany.fromString("JUH"));
+        checkSever(SvetoveStrany.fromString("sever"));
 
     }
 
     static void checkSever(SvetoveStrany s) {
-        if(s == SvetoveStrany.SEVER) {
+        if(s.jeSever) {
             System.out.println("je sever");
         } else {
             System.out.println("nie je sever");
--- src/main/java/sk/stuba/fei/uim/oop/SvetoveStrany.java
+++ src/main/java/sk/stuba/fei/uim/oop/SvetoveStrany.java
@@ -1,8 +1,22 @@
 package sk.stuba.fei.uim.oop;
 
+import java.util.Locale;
+
 public enum SvetoveStrany {
-    SEVER,
-    JUH,
-    VYCHOD,
-    ZAPAD
+    SEVER(true),
+    JUH(false),
+    VYCHOD(false),
+    ZAPAD(false);
+
+    SvetoveStrany(boolean jeSever) {
+        this.jeSever = jeSever;
+    }
+
+    boolean jeSever;
+
+    static SvetoveStrany fromString(String s) {
+        return SvetoveStrany.valueOf(s.toUpperCase(Locale.ROOT));
+    }
+
+
 }
